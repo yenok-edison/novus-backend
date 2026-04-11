6d4aa01065e0f2fd11ad2cd1f483b7e91867c412
yenok-edison <yenok@iktaraa.com>
2026-04-11 11:03:44 +0530

    base root

--- novus_backend/settings.py
+++ novus_backend/settings.py
@@ -114,17 +114,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': "novusdb",
-#         'USER': "root",
-#         'PASSWORD': "",
-#         'HOST': "localhost",
-#         'PORT': 3306
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -172,7 +161,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
